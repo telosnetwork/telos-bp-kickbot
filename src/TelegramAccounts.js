8b70348ee4830ae5976a4ad717eb7326c2eb602d
Kevin Quaintance <kevin@quaintance.net>
2021-06-15 17:01:53 -0700

    Update TelegramAccounts.js

--- src/TelegramAccounts.js
+++ src/TelegramAccounts.js
@@ -2,4 +2,11 @@ module.exports = {
     "caleosblocks": ["@foflexity"],
     "telosboston1": ["@telosboston"],
     "telosglobal1": ["@kquainta"],
+    "theteloscrew": ["@captaincryptohd"],
+    "telosunlimit": ["@macanico"],
+    "persiantelos": ["@Parsieos"],
+    "persiantelos": ["@aliiraghamii"],
+    "telosarabia1": ["@basel_alkaddoumi"],
+    "gyftieblocks ": ["@wmw509"],
+    "votetelosusa": ["@EOSUSA_Michael"],
 }
